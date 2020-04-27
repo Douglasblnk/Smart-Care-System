bc20fd679bfa00808c461ee176ce69512fa05bf0
douglasblnk <douglasblank1@gmail.com>
2020-04-26 21:45:39 -0300

    changed the api structure, create shared and services folder

---
+++ API/services/cruds/controller/Equipment/deleteEquipmentValidate.ts
@@ -0,0 +1,53 @@
+import Delete from '../../../../shared/dao/Delete';
+
+const commitData = new Delete();
+
+const TABLE = 'Equipamento';
+
+export default class DeleteEquipmentValidate {
+
+  async run(event: any) {
+    try {
+      const data = this.getData(event);
+
+      this.validateData(data);
+
+      const getQuery = this.getQuery(data)
+
+      const result = await commitData.run(getQuery);
+      console.log('cheguei até aqui');
+      return result;
+    } catch (err) {
+      console.log(err);
+
+      throw err;
+    }
+  }
+
+  getData(evt: any) {
+    const data = evt.params.id || undefined;
+
+    return data;
+  }
+
+  validateData(data: any) {
+    console.log('data cru', data);
+    
+    if (data === '' || !data) throw {
+      status: 404,
+      err: 'Equipamento não informado',
+    };
+  }
+
+  getQuery(data: any) {
+    const post = [data];
+    const query = /*SQL*/`DELETE from ${TABLE} WHERE ${TABLE}.idEquipamento = ?`
+
+    const dataQuery = { query, post, type: 'Equipamento' };
+
+    console.log(dataQuery);
+
+    return dataQuery;
+  }
+
+}
---
+++ API/services/cruds/routes/Sintoma/Sintoma.ts
@@ -0,0 +1,71 @@
+const { Router } = require("express");
+import RegisterSintomaValidate from '../../controller/Sintoma/registerSintomaValidate'
+import GetSintomaValidate from '../../controller/Sintoma/getSintomaValidate'
+import DeleteSintomaValidate from '../../controller/Sintoma/deleteSintomaValidate'
+import UpdateSintomaValidate from '../../controller/Sintoma/updateSintomavalidate'
+import Auth from '../../../../shared/auth/auth'
+
+const router = Router();
+const registerSymptom = new RegisterSintomaValidate()
+const getSymptom = new GetSintomaValidate()
+const deleteSymptom = new DeleteSintomaValidate()
+const updateSymptom = new UpdateSintomaValidate()
+const jwt = new Auth();
+
+/** 
+ *  ROTA DE CADASTRO DE SINTOMA
+ * */ 
+
+router.post('/', async (req: any, res: any) => {
+  try {
+    await jwt.jwtVerify(req)
+    const response = await registerSymptom.run(req);
+    
+    res.status(200).send(response);
+  } catch (err) {
+    console.log('deu erro mesmo', err);
+    
+    res.status(404).send(err);
+  }
+});
+
+router.get('/', async (req: any, res: any) => {
+  try {
+    await jwt.jwtVerify(req)
+    const response = await getSymptom.run(req);
+    
+    res.status(200).send(response);
+  } catch (err) {
+    console.log('deu erro mesmo', err);
+    
+    res.status(404).send(err);
+  }
+});
+
+router.delete('/:id', async (req: any, res: any) => {
+  try {
+    await jwt.jwtVerify(req)
+    const response = await deleteSymptom.run(req);
+    
+    res.status(200).send(response);
+  } catch (err) {
+    console.log('deu erro mesmo', err);
+    
+    res.status(404).send(err);
+  }
+});
+
+router.put('/:id', async (req: any, res: any) => {
+  try {
+    await jwt.jwtVerify(req)
+    const response = await updateSymptom.run(req);
+    
+    res.status(200).send(response);
+  } catch (err) {
+    console.log('deu erro mesmo', err);
+    
+    res.status(404).send(err);
+  }
+});
+
+module.exports = router;
