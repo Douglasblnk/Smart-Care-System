08bab466645999463392d5e27b6c982a8286051b
douglasblnk <douglasblank1@gmail.com>
2020-06-07 21:28:33 -0300

    feat: implemented new status endpoint

---
+++ src/services/cruds/session/status/getStatus.ts
@@ -0,0 +1,43 @@
+import statusDao from '../../dao/StatusDao';
+
+import { Connection } from 'mysql2/promise';
+import { get } from 'lodash';
+
+export default class GetStatus {
+  _queryResult: any
+
+  constructor() {
+    this._queryResult;
+  }
+  
+  private getParameters = (req: { mysql: Connection }): {
+    mysql: Connection,
+  } => ({
+    mysql: get(req, 'mysql'),
+  })
+
+  private checkParameters = ({ mysql }: { mysql?: Connection }) => ({
+    ...(!mysql ? { mysql: 'Conexão não estabelecida' } : ''),
+  })
+
+  async run(req: { mysql: Connection }) {
+    try {
+      const parameters = this.getParameters(req);
+  
+      const errors = this.checkParameters(parameters);
+      if (Object.values(errors).length > 0) throw errors;
+
+      await this.getStatus(parameters);
+
+      return this._queryResult;
+    } catch (err) {
+      console.log('err getStatus :>> ', err);
+
+      throw err;
+    }
+  }
+
+  private async getStatus(parameters: { mysql: Connection; }) {
+    this._queryResult = await new statusDao(parameters).getStatus();
+  }
+}
