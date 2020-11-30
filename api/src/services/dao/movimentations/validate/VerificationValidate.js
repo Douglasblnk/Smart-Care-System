ca6f7c7e5229cd2bd128fbbbcc36f53c0c117562
fernandocm121 <fernandocm121@users.noreply.github.com>
2020-11-30 01:28:35 -0300

    feat: validate user sequence

--- api/src/services/dao/movimentations/validate/VerificationValidate.js
+++ api/src/services/dao/movimentations/validate/VerificationValidate.js
@@ -5,18 +5,21 @@ const GenericDao = require('../../GenericDao');
 const {
   TABLE_USUARIO,
   TABLE_ORDEM_SERVICO_HAS_USUARIO,
+  TABLE_VERIFICACAO,
 } = require('../../../../shared/constants/database');
 
 module.exports = class VerificationDao extends GenericDao {
   constructor({
     cracha,
     order,
+    typeVerification,
     mysql,
   } = {}) {
     super();
 
     this._cracha = cracha;
     this._order = order;
+    this._typeVerification = typeVerification;
     this._mysql = mysql;
   }
 
@@ -26,29 +29,39 @@ module.exports = class VerificationDao extends GenericDao {
    * @return {Array} parsed array com o resultado da consulta
    */
   async validateVerification() {
-    const values = {
-      numeroCracha: parseInt(this._cracha),
-      ordemServico_idOrdemServico: this._order,
-    };
+    const [rows] = await this._mysql.query(`SELECT ${TABLE_USUARIO}.numeroCracha,
+      ${TABLE_USUARIO}.idUsuario,
+      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master,
+      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario,
+      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico
+      FROM ${TABLE_USUARIO}
+      INNER JOIN ${TABLE_ORDEM_SERVICO_HAS_USUARIO}
+      ON ${TABLE_USUARIO}.idUsuario = ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario
+      WHERE ${TABLE_USUARIO}.numeroCracha = ? AND 
+      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico = ? AND 
+      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master = ?;`,
+    [this._cracha, this._order, 1]);
 
-    const stringValue = `SELECT user_verification.numeroCracha,
-    ${TABLE_USUARIO}.idUsuario,
-    ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master,
-    ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario,
-    ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico
-    FROM ${TABLE_USUARIO} AS user_verification
-    INNER JOIN ${TABLE_ORDEM_SERVICO_HAS_USUARIO} AS users_order
-    ON ${TABLE_USUARIO}.idUsuario = ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario
-    WHERE ${TABLE_USUARIO}.numeroCracha = ? AND
-    ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico = ? AND
-    ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master = 1;`;
+    return this.parseSelectResponse(rows);
+  }
 
-    console.log('stringValue: ', stringValue);
-    console.log('Values validateVerification: ', values);
+  async validateRegisterExist() {
+    const [rows] = await this._mysql.query(`
+      SELECT * FROM ${TABLE_VERIFICACAO} 
+      WHERE Verificacao.ordemServico_idOrdemServico = ? AND
+      Verificacao.tipoVerificacao = ?;`,
+    [this._order, this._typeVerification]);
 
-    const rows = await this._mysql.query(/* SQL */stringValue, [values]);
-    console.log('rows: ', rows);
-    
-    return this.parseInsertResponse(rows);
+    return this.parseSelectResponse(rows);
+  }
+
+  async validateCheckPreviousSequence(typeVerification) {
+    const [rows] = await this._mysql.query(`
+      SELECT * FROM ${TABLE_VERIFICACAO} 
+      WHERE Verificacao.ordemServico_idOrdemServico = ? AND
+      Verificacao.tipoVerificacao = ?;`,
+    [this._order, typeVerification]);
+
+    return this.parseSelectResponse(rows);
   }
 };
