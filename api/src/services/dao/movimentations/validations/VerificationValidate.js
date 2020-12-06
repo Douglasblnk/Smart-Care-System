7cc6bab47c051768f0b5c1d820211d48ffd338fd
douglasblnk <douglasblank1@gmail.com>
2020-12-06 20:39:56 -0300

    indent

--- api/src/services/dao/movimentations/validations/VerificationValidate.js
+++ api/src/services/dao/movimentations/validations/VerificationValidate.js
@@ -29,38 +29,43 @@ module.exports = class VerificationDao extends GenericDao {
    * @return {Array} parsed array com o resultado da consulta
    */
   async validateVerification() {
-    const [rows] = await this._mysql.query(`SELECT ${TABLE_USUARIO}.numeroCracha,
-      ${TABLE_USUARIO}.idUsuario,
-      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master,
-      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario,
-      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico
+    const [rows] = await this._mysql.query(/* SQL */`
+      SELECT
+        ${TABLE_USUARIO}.numeroCracha,
+        ${TABLE_USUARIO}.idUsuario,
+        ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master,
+        ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario,
+        ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico
       FROM ${TABLE_USUARIO}
-      INNER JOIN ${TABLE_ORDEM_SERVICO_HAS_USUARIO}
-      ON ${TABLE_USUARIO}.idUsuario = ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario
-      WHERE ${TABLE_USUARIO}.numeroCracha = ? AND 
-      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico = ? AND 
-      ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master = ?;`,
-    [this._cracha, this._order, 1]);
+        INNER JOIN ${TABLE_ORDEM_SERVICO_HAS_USUARIO} ON ${TABLE_USUARIO}.idUsuario = ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.Usuario_idUsuario
+      WHERE ${TABLE_USUARIO}.numeroCracha = ?
+        AND  ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.ordemServico_idOrdemServico = ?
+        AND ${TABLE_ORDEM_SERVICO_HAS_USUARIO}.is_master = ?;
+    `, [this._cracha, this._order, 1]);
 
     return this.parseSelectResponse(rows);
   }
 
   async validateRegisterExist() {
-    const [rows] = await this._mysql.query(`
-      SELECT * FROM ${TABLE_VERIFICACAO} 
-      WHERE Verificacao.ordemServico_idOrdemServico = ? AND
-      Verificacao.tipoVerificacao = ?;`,
-    [this._order, this._typeVerification]);
+    const [rows] = await this._mysql.query(/* SQL */`
+      SELECT
+        *
+      FROM ${TABLE_VERIFICACAO} 
+      WHERE Verificacao.ordemServico_idOrdemServico = ?
+        AND Verificacao.tipoVerificacao = ?;
+    `, [this._order, this._typeVerification]);
 
     return this.parseSelectResponse(rows);
   }
 
   async validateCheckPreviousSequence(typeVerification) {
-    const [rows] = await this._mysql.query(`
-      SELECT * FROM ${TABLE_VERIFICACAO} 
-      WHERE Verificacao.ordemServico_idOrdemServico = ? AND
-      Verificacao.tipoVerificacao = ?;`,
-    [this._order, typeVerification]);
+    const [rows] = await this._mysql.query(/* SQL */`
+      SELECT
+        *
+      FROM ${TABLE_VERIFICACAO}
+      WHERE Verificacao.ordemServico_idOrdemServico = ?
+        AND Verificacao.tipoVerificacao = ?;
+    `, [this._order, typeVerification]);
 
     return this.parseSelectResponse(rows);
   }
